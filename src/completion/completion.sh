4abdfcda989673585d45bf7e9fe3e8ad0b08a667
Caio Ronchi <142912765+ronchi-oss@users.noreply.github.com>
2024-02-10 14:46:36 +0100

    refactor: no reason to overcomplicate this.

--- src/completion/completion.sh
+++ src/completion/completion.sh
@@ -5,12 +5,5 @@ __chpath_completion__suggest_commands() {
 }
 
 __chpath_completion__suggest_commands_with_help() {
-    __chpath_completion__suggest_commands \
-        | while read -r command; do
-              line="$(type "__chpath_command__${command}__help" 2>/dev/null | head -n 1)"
-              if test "${line##* }" != 'function'; then
-                  continue
-              fi
-              echo "$command"
-          done
+    __chpath_completion__suggest_commands
 }
